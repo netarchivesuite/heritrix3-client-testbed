686071d1902fa07e9518e464e223ec003dce0437
Colin Rosenthal <csr@statsbiblioteket.dk>
2014-09-22 09:51:24 +0200

    Added example of client without keystore.

--- src/test/java/dk/netarkivet/heritrix3/rest/RestClientImplTest.java
+++ src/test/java/dk/netarkivet/heritrix3/rest/RestClientImplTest.java
@@ -10,6 +10,9 @@ import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,14 +21,16 @@ import java.security.cert.X509Certificate;
 
 public class RestClientImplTest {
 
-
+    /**
+     * This test uses a keystore containing the h3 self-signed key.
+     * @throws Exception
+     */
     @org.testng.annotations.Test
-    public void testGetEngine() throws Exception {
+    public void testGetEngineWithKeystore() throws Exception {
         int port = 8443;
         String hostname = "pc609.sb.statsbiblioteket.dk";
         String userName = "admin";
         String password = "admin";
-
         KeyStore trust;
         SSLSocketFactory socketFactory;
         Scheme sch;
@@ -46,4 +51,47 @@ public class RestClientImplTest {
         System.out.println(restClient.getEngine());
 
     }
+
+    /**
+     * In this test, we connect to the h3 instance using a TrustManager that accepts any certificate, so there is no
+     * need for a keystore.
+     * @throws Exception
+     */
+    @org.testng.annotations.Test
+      public void testGetEngineWithoutKeystore() throws Exception {
+          int port = 8443;
+          String hostname = "pc609.sb.statsbiblioteket.dk";
+          String userName = "admin";
+          String password = "admin";
+          // Create a trust manager that does not validate certificate chains
+          TrustManager[] trustAllCerts = new TrustManager[] {
+              new X509TrustManager() {
+                  public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                      return new X509Certificate[0];
+                  }
+                  public void checkClientTrusted(
+                      java.security.cert.X509Certificate[] certs, String authType) {
+                      }
+                  public void checkServerTrusted(
+                      java.security.cert.X509Certificate[] certs, String authType) {
+                  }
+              }
+          };
+          SSLSocketFactory socketFactory;
+          Scheme sch;
+          HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+          SSLContext sc = SSLContext.getInstance("SSL");
+          sc.init(null, trustAllCerts, new java.security.SecureRandom());
+          socketFactory = new SSLSocketFactory(sc);
+          socketFactory.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
+          sch = new Scheme("https", socketFactory, port);
+          DefaultHttpClient client = new DefaultHttpClient();
+          client.getConnectionManager().getSchemeRegistry().register(sch);
+          client.getCredentialsProvider().setCredentials(new AuthScope(hostname, port),
+                  new UsernamePasswordCredentials(userName, password));
+          String baseUrl = "https://" + hostname + ":" + Integer.toString(port) + "/engine/";
+          RestClient restClient = new RestClientImpl(client, baseUrl);
+          System.out.println(restClient.getEngine());
+      }
+
 }
